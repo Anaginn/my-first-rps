aca4e01e91c70f747d545b56e06f48da4bba7be7
admin <prudnikova_nastenka1@mail.ru>
2023-11-29 01:31:27 +0300

    first commit

---
+++ massiv/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class SortedArray(models.Model):
+    array_name = models.CharField(max_length=100)
+    sorted_array = models.TextField()
+
+    def __str__(self):
+        return self.array_name
